38b25e77805686358e345495703ee9b5ca9771cb
BrownEdgar <nikoghosjanedgar@gmail.com>
2023-09-08 22:20:37 +0400

    added multer example save image

--- server/app.js
+++ server/app.js
@@ -21,44 +21,43 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(
-	cookieSession({ name: "session", keys: ["zidan"], maxAge: 24 * 60 * 60 * 1000 })
+  cookieSession({ name: "session", keys: ["zidan"], maxAge: 24 * 60 * 60 * 1000 })
 );
 app.use(cors({
-	origin: 'http://localhost:5173/',
-	methods: "GET POST PUT PATCH DELETE",
-	credentials: true
+  origin: '*',
+  credentials: true
 }))
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static('uploads'));
 
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/cars', carsRouter);
 
 mongoose.connect(process.env.MONGO_DB_URL)
-.then(() => console.log("welcome to Mongo DB"))
-.catch(err =>  console.log(err))
+  .then(() => console.log("welcome to Mongo DB"))
+  .catch(err => console.log(err))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
--- server/routes/cars.js
+++ server/routes/cars.js
@@ -2,7 +2,25 @@ const express = require('express');
 const CarController = require('../controllers/CarController');
 const router = express.Router();
 const controller = new CarController()
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    console.log(`file: `, file)
+    const uniqueSuffix = Math.round(Math.random() * 1E9)
+    cb(null, `photo-${uniqueSuffix}.${file.mimetype.split('/')[1]}`)
+  }
+})
+
+const upload = multer({ storage: storage })
+
+
 /* GET home page. */
 router.get('/', controller.getAll);
+router.get('/', controller.getCarsById);
+router.post('/', upload.single('img'), controller.addCar);
 
 module.exports = router;
